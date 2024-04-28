ac56524fd303e805ab1698b3d8bf50bab930c87f
Sanket Singh <singhsanket143@gmail.com>
2024-04-28 12:00:31 +0530

    Added codes

---
+++ 11.BinaryTree/BT.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null; 
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+}
