53b9570195708f7b29468de69ae63c76df70fe91
Sanket Singh <singhsanket143@gmail.com>
2024-04-28 10:32:59 +0530

    Added codes

--- 10.Deque/lrucache.js
+++ 10.Deque/lrucache.js
@@ -1,6 +1,7 @@
 class Node {
-    constructor(data) {
-        this.data = data;
+    constructor(key, value) {
+        this.data = key;
+        this.value = value;
         this.next = null;
         this.prev = null;
     }
@@ -16,38 +17,40 @@ class DoublyLinkedList {
         return this.head == null;
     }
 
-    addAtHead(data) {
+    addAtHead(key, value) {
         // 1. Is the ll is empty, create a new node and assign it as head and tail both
         if(this.isEmpty()) {
             // a. Create a new node
-            const newNode = new Node(data);
+            const newNode = new Node(key, value);
             // b. Assign this node as the head
             this.head = newNode;
             // c. Assign the same node as tail
             this.tail = newNode;
+            return newNode;
         } else {
             // 2. If the list is not empty, then create a new node and attach it behind the old head
             
             // a. Create a new node
-            const newNode = new Node(data);
+            const newNode = new Node(key, value);
             // b. attach behind the old head by making next of newNode equal to old head
             newNode.next = this.head;
             // c. Connect the prev of the old head to the newNode
             this.head.prev = newNode;
             // d. Make the newNode as the new head of the list
             this.head = newNode;
+             return newNode;
         }
     }
 
-    addAtTail(data) {
+    addAtTail(key, value) {
         if(this.isEmpty()) {
             // 1. If the LL is empty then adding at head is same as adding at tail
-            this.addAtHead(data);
+            this.addAtHead(key, value);
         } else {
             // 2. LL is not empty
 
             // a. Create a new node
-            const newNode = new Node(data);
+            const newNode = new Node(key, value);
             // b. Setup the conn
             this.tail.next = newNode;
             newNode.prev = this.tail;
@@ -146,17 +149,58 @@ class DoublyLinkedList {
 
 }
 
-const dll = new DoublyLinkedList();
-dll.addAtHead(10);
-dll.addAtHead(9);
-dll.addAtHead(8);
-dll.addAtTail(11);
-dll.addAtTail(12);
-dll.removeAtHead();
-dll.removeAtTail();
-dll.removeAtHead();
-dll.removeAtTail();
-dll.removeAtHead();
-dll.removeAtTail();
-// 8 - 9 - 10 - 11 - 12
-dll.display();
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.dll = new DoublyLinkedList();
+    this.cacheCapacity = capacity;
+    this.size = 0; // current cache size
+    this.mp = {};
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if(this.mp[key] == undefined) return -1;
+    const result = this.mp[key].value;
+    const nodeTobeRemoved = this.mp[key];
+    this.dll.removeNode(nodeTobeRemoved);
+    this.mp[key] = this.dll.addAtHead(key, result);
+    return result;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if(this.mp[key] != undefined) {
+        // the key we wanted to add already exist
+        const nodeTobeRemoved = this.mp[key];
+        this.dll.removeNode(nodeTobeRemoved);
+        this.mp[key] = this.dll.addAtHead(key, value);
+    } else {
+        // a new key value pair is being added
+        if(this.size == this.cacheCapacity) {
+            // the cache is full
+            // remove the lru element
+            delete this.mp[this.dll.tail.data];
+            this.dll.removeAtTail();
+            this.size--;
+        }
+        this.mp[key] = this.dll.addAtHead(key, value);
+        this.size++;
+    }
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
