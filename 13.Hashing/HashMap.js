35a057ab49deb9ef40b58bc725a6834a5da2df46
Sanket Singh <singhsanket143@gmail.com>
2024-05-26 10:39:51 +0530

    added codes

--- 13.Hashing/HashMap.js
+++ 13.Hashing/HashMap.js
@@ -1,8 +1,9 @@
 // 1. Structure that should contain array of linked list
 // 2. Load factor , at any point of time load factor > 0.5 we rehash
 // Java - Hashmap, C++ - unordered_map , python - dict, js - objects
-var Node = function(data) {
-    this.data = data;
+var Node = function(key, value) {
+    this.key = key; // key
+    this.value = value;
     this.next = null; // until or unless we attach this new node to a list, the next property should be null
 }
 
@@ -12,128 +13,32 @@ var MyLinkedList = function() {
 };
 
 /** 
- * @param {number} index
- * @return {number}
- */
-MyLinkedList.prototype.get = function(index) {
-    let i = 0;
-    let temp = this.head;
-    while(i < index && temp != null) {
-        i++;
-        temp = temp.next;
-    }
-    if(temp == null) return -1;
-    return temp.data;
-};
-
-/** 
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtHead = function(val) {
+MyLinkedList.prototype.addAtHead = function(key, val) {
     if(this.head == null) {
         // means the ll is empty, so this node should become the head
-        this.head = new Node(val);
+        this.head = new Node(key, val);
     } else {
         // when ll is not. empty
-        let n = new Node(val);
+        let n = new Node(key,  val);
         n.next = this.head;
         this.head = n;
     }
 };
 
-/** 
- * @param {number} val
- * @return {void}
- */
-MyLinkedList.prototype.addAtTail = function(val) {
-    if(this.head == null) {
-        // ll is empty
-        this.head = new Node(val);
-    } else {
-        let temp = this.head;
-        while(temp.next != null) {
-            temp = temp.next;
-        }
-        //. when the loop ends, temp points at the tail node
-        let n = new Node(val);
-        temp.next = n;
-        // that's it
-    }
-};
-
-/** 
- * @param {number} index 
- * @param {number} val
- * @return {void}
- */
-MyLinkedList.prototype.addAtIndex = function(index, val) {
-    if(this.head == null && index != 0) return;
-    if(this.head == null ) {
-       // console.log("hello")
-        this.head = new Node(val);
-        return;
-    }
-    if(index == 0) {
-        this.addAtHead(val);
-        return;
-    }
-    let i = 0;
-    let prev = null;
-    let curr = this.head;
-    while(i < index && curr != null) {
-        i++;
-        prev = curr;
-        curr = curr.next;
-    }
-    if(i != index) return;
-    let n = new Node(val);
-    prev.next = n;
-    n.next = curr;
-    return;
-};
-
-/** 
- * @param {number} index
- * @return {void}
- */
-MyLinkedList.prototype.deleteAtIndex = function(index) {
-    if(this.head == null) return;
-    if(index == 0) {
-        this.deleteAtHead();
-        return;
-    }
-    let i = 0;
-    let prev = null;
-    let nodeToBeDel = this.head;
-    while(i < index && nodeToBeDel != null) {
-        prev = nodeToBeDel;
-        nodeToBeDel = nodeToBeDel.next;
-        i++;
-    }
-    if(i != index) {
-        // ll was not having the index
-        return;
-    }
-    if(nodeToBeDel == null) return;
-    let newNext = nodeToBeDel.next;
-    prev.next = newNext;
-    nodeToBeDel.next = null;  // this now will be later garbage collected
-    return;
-};
-
-MyLinkedList.prototype.deleteAtHead = function() {
-    if(this.head == null) return;
-    let nextHead = this.head.next;
-    let nodeToBeDelete = this.head; // curr head
-    this.head = nextHead;
-    nodeToBeDelete.next = null; // prev head disconnected
-}
 class HashMap {
     constructor() {
         this.lambdaFactorThreshold = 0.5; // threshold lambda factor
-        this.arr = [];
         this.maxSize = 2; // how many buckets are avaialable
+
+        this.arr = Array(this.maxSize);
+
+        for(let i = 0; i < this.maxSize; i++) {
+            this.arr[i] = new MyLinkedList(); // store empty LL at each index
+        }
+
         this.currSize = 0; // how many elements are inserted
     }
 
@@ -157,7 +62,7 @@ class HashMap {
         let C = this.maxSize;
         for(let i = 0; i < key.length; i++) {
             // go to each character of given
-            let asciiValue = ascii(key, i);
+            let asciiValue = this.ascii(key, i);
             // ans = ((ans%this.currSize) + ((asciiValue%this.currSize)*(pow%this.currSize))%this.currSize) % this.currSize;
 
             ans = ( (ans%C)+ ((asciiValue%C)*(pow%C))%C ) % C;
@@ -175,12 +80,91 @@ class HashMap {
     ascii(key, index) {
         return key.charCodeAt(index);
     }
-}
+
+    display() {
+        for(let i = 0; i < this.arr.length; i++) {
+            let temp = this.arr[i].head;
+            let str = "LL: ";
+            while(temp != null) {
+                str += "(" + temp.key + ", " + temp.value  + ") -> ";
+                temp = temp.next;
+            }
+            console.log(str);
+        }
+        console.log(this.currSize, this.maxSize);
+        console.log("*******")
+    }
+
+    insert(key, value) {
+        // TODO: Implement update part
+
+        let newLoadFactor = (this.currSize + 1) / this.maxSize;
+        if(newLoadFactor > this.lambdaFactorThreshold) {
+            // if after insertion of new (key, value) pair the load factor will go beyonf the thresold
+            // we should first rehash and then insert
+            this.rehash(); 
+        }
+
+        const bucketIndex = this.hashFunction(key); // hash value generated by the function is the bucket index of the arr only
+
+        this.arr[bucketIndex].addAtHead(key, value);
+        this.currSize += 1; // new pair added so size of hashmap increases
+
+    }
+
+    remove(key) {
+
+    }
+
+    search(key) {
+        
+    }
+
+    rehash() {
+        this.maxSize *= 2; // double the capacity of the arr
+
+        const newArr = Array(this.maxSize);// new array with updated capacity
+
+        for(let i = 0; i < this.maxSize; i++) {
+            newArr[i] = new MyLinkedList();
+        }
+
+        const oldArr = this.arr; 
+
+        // Iterate on each node of every LL and insert them in the newArr
+
+        for(let i = 0; i < oldArr.length; i++) {
+            // go to each bucket of old array
+            // each bucket has a LL
+
+            let temp = oldArr[i].head;
+            while(temp != null) {
+                let key = temp.key;
+                let value = temp.value;
+                
+
+                const bucketIndex = this.hashFunction(key);
+
+                newArr[bucketIndex].addAtHead(key, value);
+
+                temp = temp.next;
+            }
+        }
+
+        this.arr = newArr;
+    }
+ }
 
 
-/**
- * 
- * [[], []], lambda > 0.5
- * 
- * [[], [], [], []]
- */
+ const hm = new HashMap();
+
+ hm.insert("mango", 10);
+ hm.display();
+ hm.insert("banana", 3);
+ hm.display();
+ hm.insert("apple", 4);
+ hm.display();
+ hm.insert("grapes", 14);
+ hm.display();
+ hm.insert("xyz", 14);
+ hm.display();
